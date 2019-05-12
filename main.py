9083ba6f4eac68d3c88a70bbe4fab140e540842e
Roman Sarkisyan <sarkisyan.r.a@yandex.ru>
2019-05-12 18:55:48 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.properties import ObjectProperty
+from kivy.core.window import Window
+from kivy.config import Config
+
+Config.set('kivy','keyboard_mode','systemanddock')
+#360x640 - Комп, 1080x1920 - Смарт
+Window.size = (1080,1920)
+
+class Container(GridLayout):
+    texin = ObjectProperty()
+    lain = ObjectProperty()
+
+    def change_text(self):
+        self.lain.text = self.texin.text
+
+
+class MyApp(App):
+    def build(self):
+        
+        return Container()
+
+if __name__ == '__main__':
+    MyApp().run()
